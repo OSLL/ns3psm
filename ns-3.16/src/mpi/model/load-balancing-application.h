e2bee6fb80dc9ee03714d79d68a2549a1915d94e
lesnova <lesnovaolga@gmail.com>
2013-06-08 20:47:03 +0400

    repartition debug18

--- ns-3.16/src/mpi/model/load-balancing-application.h
+++ ns-3.16/src/mpi/model/load-balancing-application.h
@@ -58,7 +58,7 @@ struct graph_t
   parmetis_idx_t nparts;
   parmetis_real_t* tpwgts;
   parmetis_idx_t* part;
-  parmetis_idx_t* part_all;
+  int* part_all;
 
   parmetis_idx_t wgtflag;
   parmetis_idx_t numflag;
